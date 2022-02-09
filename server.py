09cf22674ff005f022c736b2104b1d1b665418b8
Anna Atkinson <atkinsonsoftware@gmail.com>
2022-02-08 16:43:47 -0800

    Add POST handler.

--- server.py
+++ server.py
@@ -15,6 +15,15 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        logging.info("POST, Path: %s, Headers: %s,\nBody:%s",
+            str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, port), MyServer)
     print("Server started http://%s:%s" % (hostName, port))
